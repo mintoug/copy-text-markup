7552b858809c20d28d655d5c91f00e7c4388b90e
Mandhouj A <nouitou@yahoo.fr>
2021-10-19 18:10:57 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const btn = document.querySelector('.btn');
+const coupon = document.querySelector('.coupon');
+
+const copyText = (e) => {
+    e.preventDefault();
+       coupon.select();
+       coupon.setSelectionRange(0, 99999);
+       document.execCommand("copy");
+
+       btn.textContent = "copied!!!!";
+       setTimeout(() =>{
+           btn.textContent ="copy";
+       }, 3000);
+
+  
+};
+
+;btn.addEventListener('click', copyText);
